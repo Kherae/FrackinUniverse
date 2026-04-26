3f43eae9f4f862caa9e060e2273e275000868ca5
Kherae <cwaldridge@live.com>
2026-04-25 19:23:13 -0700

    Update burningnapalm.lua

--- stats/effects/fu_weaponeffects/burningnapalm/burningnapalm.lua
+++ stats/effects/fu_weaponeffects/burningnapalm/burningnapalm.lua
@@ -1,36 +1,36 @@
 function init()
-  animator.setParticleEmitterOffsetRegion("flames", mcontroller.boundBox())
-  animator.setParticleEmitterActive("flames", true)
-  effect.setParentDirectives("fade=BF3300=0.25")
+	animator.setParticleEmitterOffsetRegion("flames", mcontroller.boundBox())
+	animator.setParticleEmitterActive("flames", true)
+	effect.setParentDirectives("fade=BF3300=0.25")
 
-  script.setUpdateDelta(5)
+	script.setUpdateDelta(5)
 
-  self.tickDamagePercentage = 0.045
-  self.tickTime = 0.5
-  self.tickTimer = self.tickTime
+	self.tickDamagePercentage = 0.045
+	self.tickTime = 0.5
+	self.tickTimer = self.tickTime
 end
 
 function update(dt)
-  if effect.duration() and world.liquidAt({mcontroller.xPosition(), mcontroller.yPosition() - 1}) then
-    effect.expire()
-  end
+	if effect.duration() and world.liquidAt({mcontroller.xPosition(), mcontroller.yPosition() - 1}) then
+		effect.expire()
+	end
 
-  self.tickTimer = self.tickTimer - dt
-  	local damageVal
+	self.tickTimer = self.tickTimer - dt
+	local damageVal
 	if status.statPositive("specialStatusImmunity") then
 		damageVal=math.floor(world.threatLevel() * self.tickDamagePercentage * 100)
 	else
 		damageVal=math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1
 	end
-  if self.tickTimer <= 0 then
-    self.tickTimer = self.tickTime
-    status.applySelfDamageRequest({
-        damageType = "IgnoresDef",
-        damage = damageVal,--math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1,
-        damageSourceKind = "fire",
-        sourceEntityId = entity.id()
-      })
-  end
+	if self.tickTimer <= 0 then
+		self.tickTimer = self.tickTime
+		status.applySelfDamageRequest({
+			damageType = "IgnoresDef",
+			damage = damageVal,--math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1,
+			damageSourceKind = "fire",
+			sourceEntityId = entity.id()
+		})
+	end
 end
 
 function uninit()
