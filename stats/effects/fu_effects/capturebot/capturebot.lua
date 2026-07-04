01a2f8ad15c9f42889bf283094dd105a5407889e
Kherae <cwaldridge@live.com>
2026-07-03 22:43:14 -0700

    mega capture bot sans newsprites

--- stats/effects/fu_effects/capturebot/capturebot.lua
+++ stats/effects/fu_effects/capturebot/capturebot.lua
@@ -1,40 +1,66 @@
---no longer used
--- function init()
-	-- if status.statPositive("captureImmunity") or status.statPositive("specialStatusImmunity") then
+--only used on the mega capture bot now. was too broken otherwise.
+function init()
+	if world.entityType(entity.id()) ~= "monster" then
+		effect.expire()
+		return
+	end
+
+	local pass,result=pcall(world.callScriptedEntity,entity.id(),"config.getParameter","capturable")
+	self.isCapturable=pass and result
+
+	if not self.isCapturable or status.statPositive("captureImmunity") or status.statPositive("specialStatusImmunity") then
+		effect.expire()
+		return
+	end
+
+	effect.setParentDirectives(config.getParameter("directives", ""))
+	self.healthPercentage = config.getParameter("healthPercentage", 0.1)
+
+	effect.addStatModifierGroup({{stat = "deathbombDud", amount = 1}})
+
+	if (status.isResource("stunned")) and (not status.statPositive("stunImmunity")) and (not status.isResource("food")) then
+		mcontroller.controlModifiers({facingSuppressed = true,movementSuppressed = true})
+		status.setResource("stunned", math.max(status.resource("stunned"), effect.duration()))
+		mcontroller.setVelocity({0, 0})
+	end
+end
+
+function update(dt)
+	if world.entityType(entity.id()) ~= "monster" then
+		effect.expire()
+		return
+	end
+
+	local pass,result=pcall(world.callScriptedEntity,entity.id(),"config.getParameter","capturable")
+	self.isCapturable=pass and result
+
+	if not self.isCapturable or status.statPositive("captureImmunity") or status.statPositive("specialStatusImmunity") then
+		effect.expire()
+		return
+	end
+
+	status.setResourcePercentage("health", math.min(status.resourcePercentage("health"), self.healthPercentage))
+
+	if (status.isResource("stunned")) and (not status.statPositive("stunImmunity")) and (not status.isResource("food")) then
+		mcontroller.controlModifiers({facingSuppressed = true,movementSuppressed = true})
+		status.setResource("stunned", math.max(status.resource("stunned"), effect.duration()))
+		mcontroller.setVelocity({0, 0})
+	end
+end
+
+function uninit()
+	if world.entityType(entity.id()) ~= "monster" then
 		-- effect.expire()
-		-- return
-	-- end
-	-- effect.setParentDirectives(config.getParameter("directives", ""))
-	-- self.healthPercentage = config.getParameter("healthPercentage", 0.1)
+		return
+	end
 
-	-- effect.addStatModifierGroup({{stat = "deathbombDud", amount = 1}})
+	local pass,result=pcall(world.callScriptedEntity,entity.id(),"config.getParameter","capturable")
+	self.isCapturable=pass and result
 
-	-- if (status.isResource("stunned")) and (not status.statPositive("stunImmunity")) and (not status.isResource("food")) then
-		-- mcontroller.controlModifiers({facingSuppressed = true,movementSuppressed = true})
-		-- status.setResource("stunned", math.max(status.resource("stunned"), effect.duration()))
-		-- mcontroller.setVelocity({0, 0})
-	-- end
--- end
-
--- function update(dt)
-	-- if world.entityType(entity.id()) ~= "monster" or status.statPositive("captureImmunity") or status.statPositive("specialStatusImmunity") then
+	if not self.isCapturable or status.statPositive("captureImmunity") or status.statPositive("specialStatusImmunity") then
 		-- effect.expire()
-		-- return
-	-- else
-		-- status.setResourcePercentage("health", math.min(status.resourcePercentage("health"), self.healthPercentage))
+		return
+	end
 
-		-- if (status.isResource("stunned")) and (not status.statPositive("stunImmunity")) and (not status.isResource("food")) then
-			-- mcontroller.controlModifiers({facingSuppressed = true,movementSuppressed = true})
-			-- status.setResource("stunned", math.max(status.resource("stunned"), effect.duration()))
-			-- mcontroller.setVelocity({0, 0})
-		-- end
-	-- end
--- end
-
--- function uninit()
-	-- if world.entityType(entity.id()) ~= "monster" or status.statPositive("captureImmunity") or status.statPositive("specialStatusImmunity") then
-		-- return
-	-- elseif (status.isResource("stunned")) and (not status.statPositive("stunImmunity")) and (not status.isResource("food")) then
-		-- status.setResource("stunned", 0)
-	-- end
--- end
+	status.setResource("stunned", 0)
+end
